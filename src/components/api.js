ebe5f8f297579321c2c549196864c9b45c77b275
Anna Zatsepina <suv.ann123@yandex.ru>
2023-04-25 15:48:48 +0300

    first-part-cards

--- src/components/api.js
+++ src/components/api.js
@@ -1,3 +1,36 @@
+export default class Api {
+  constructor(config) {
+    this._baseUrl = config.url;
+    this._headers = config.headers;
+  }
+
+  makeRequest(path) {
+    return fetch(this._url + path, {
+      method: "GET",
+      headers: this._headers,
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Ошибка: ${res.status}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+
+  } };
+
+
+  const config = new Api({
+    baseUrl: 'https://nomoreparties.co/v1/cohort-42',
+    headers: {
+      authorization: 'c56e30dc-2883-4270-a59e-b2f7bae969c6',
+      'Content-Type': 'application/json'
+    }
+  }); 
+
+////////////////////
 const config = {
   baseUrl: "https://nomoreparties.co/v1/plus-cohort-22",
   headers: {
